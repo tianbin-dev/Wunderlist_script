e72c4c1330b9576e619b1397c4f2802c54f8d907
tianbin <tianbin@917@gmail.com>
2016-10-20 18:20:13 +0800

    [Fixed bug] Desc:

--- subtask.py
+++ subtask.py
@@ -13,13 +13,18 @@ class Subtask:
     def __str__(self):
         return self.title
 
-    def format(self):
+    def format_when_contain_url(self):
         subtask_title = ''
+        
         urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', self.title)
+        
         if len(urls) > 0:
+            
             url_start_index = self.title.index(urls[0])
             subtask_title += str(self.title[0:url_start_index]).strip()
-        for url in urls:
-            subtask_title += '\n'+str(url).strip()
-        self.title = subtask_title
+            
+            for url in urls:
+                subtask_title += '\n'+str(url).strip()
+            
+            self.title = subtask_title
 
